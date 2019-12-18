529e817cf5d4d1f4ff31b35c70cb9884586b58bb
onedayrex <1115757866@qq.com>
2019-12-18 09:39:16 +0800

    :bulb: 添加说明

--- src/main/java/com/git/onedayrex/designpattern/proxy/dynamic/cglibproxy/AnimalCglibProxy.java
+++ src/main/java/com/git/onedayrex/designpattern/proxy/dynamic/cglibproxy/AnimalCglibProxy.java
@@ -10,6 +10,7 @@ public class AnimalCglibProxy implements MethodInterceptor {
     private Enhancer enhancer = new Enhancer();
 
     public Object getProxy(Class<?> clazz) {
+        //通过Enhancer生成代理对象
         enhancer.setSuperclass(clazz);
         enhancer.setCallback(this);
         return enhancer.create();
@@ -17,6 +18,7 @@ public class AnimalCglibProxy implements MethodInterceptor {
 
     @Override
     public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+        //具体的代理实现
         System.out.println("==>cglib proxy before method");
         Object o = proxy.invokeSuper(obj, args);
         System.out.println("==>cglib proxy after method");
